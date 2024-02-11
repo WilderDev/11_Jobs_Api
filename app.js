171ef2fa9de5e7600c2a05a6d1a29da557fa11c4
WilderDev <williamjohnwilder@outlook.com>
2024-02-11 12:06:38 -0600

    MONGO

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const connectToMongo = require('./lib/mongoose');
+
 // * MIDDLEWARE
 app.use(cors()); // CORS
 app.use(express.json()); // Body Parser
@@ -16,10 +18,10 @@ app.use('/api/v1/auth', require('./routes/auth.routes'));
 // * START SERVER & DB
 (async () => {
   try {
-    // TODO: Start Database
+    await connectToMongo(process.env.MONGO_URI); // Start Database
 
     const PORT = process.env.PORT || 5000;
-    const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5000';
+    const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
 
     app.listen(PORT, () => console.log(`Server Listening: ${SERVER_URL}`)); // Start Server
   } catch (err) {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,10 @@
 const router = require('express').Router();
 
 // * ROUTES
+// TODO: REGISTER
+router.post('/');
+
+// TODO: LOGIN
 
 // * EXPORTS
 module.exports = router;
