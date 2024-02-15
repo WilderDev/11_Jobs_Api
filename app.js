1d74249c1beb80eeb50d5ecd2565c82917532cd9
WilderDev <williamjohnwilder@outlook.com>
2024-02-15 12:36:49 -0600

    START(class 2)

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 require('express-async-errors');
 
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 const connectToMongo = require('./lib/mongoose');
@@ -11,9 +10,23 @@ const { PORT, SERVER_URL } = require('./lib/constants');
 
 const authMiddleware = require('./middleware/auth.middleware');
 
+// SECURITY
+const helment = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // * MIDDLEWAREs
+app.use(xss()); // XSS
 app.use(cors()); // CORS
+app.use(helment()); // Header Security
 app.use(express.json()); // Body Parser
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 Minutes
+    max: 100, // limit each IP to 100 requests per window (15 mins)
+  }),
+); // Rate Limited (Prevents Brute Force Attacks)
 
 // * ROUTES
 app.use('/api/v1/auth', require('./routes/auth.routes'));
